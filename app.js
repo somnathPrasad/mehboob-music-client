e1f1cc2bd07f6a9be0f83b01abeaed689b4dd521
Somnath Prasad <somnathprasad559@gmail.com>
2021-09-15 18:33:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const ejs = require("ejs")
+const port = process.env.PORT || 3000
+const app = express()
+app.use(express.json())
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+app.listen(port,()=>{
+    console.log(`Mehboob music official listining on port ${port}`)
+})
